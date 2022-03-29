0fe0c0e8262f04ecef5c043d2549aee5ad627679
shao ting hsiao <1803098@narlabs.org.tw>
2022-03-29 14:39:21 +0800

    L5 Entry Point

---
+++ Hazel/src/Hazel/Application.cpp
@@ -0,0 +1,16 @@
+#include "Application.h"
+#include <iostream>
+namespace Hazel {
+	Application::Application()
+	{
+	}
+
+	Application::~Application()
+	{
+	}
+	void Application::Run()
+	{
+		while (true);
+	}
+
+}
